aebe9bd738598bafd5a42adff22eb11034b050a6
Durbek <pattoyevd@gmail.com>
2022-11-05 22:04:56 +0500

    comment

--- comment/models.py
+++ comment/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(vr, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
