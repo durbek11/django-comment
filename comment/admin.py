39213c4624f422b0d679cad1334b64f1de2b833a
Durbek <pattoyevd@gmail.com>
2022-12-25 21:50:01 +0500

    comment

--- comment/admin.py
+++ comment/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Post, Comment
 
-admin.site.register(Post)
+admin.site.register(Product)
 admin.site.register(Comment)
--- comment/urls.py
+++ comment/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 app_name = 'comment'
 
 urlpatterns = [
+    path('', home, name="home")
 ]
