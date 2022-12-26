f4aae735d6963460ca01744ab0443084d3eb2e8a
Durbek <pattoyevd@gmail.com>
2022-12-26 17:36:40 +0500

    bug fixed

--- comment/admin.py
+++ comment/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib import admin
-from .models import Product, Comment
+from .models import Product, Comment, MyUser
 
+admin.site.register(MyUser)
 admin.site.register(Product)
 admin.site.register(Comment)
--- core/settings.py
+++ core/settings.py
@@ -108,5 +108,5 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
+AUTH_USER_MODEL = 'comment.MyUser'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
