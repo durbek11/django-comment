e80813e2b44d458569f245eb3ed8fa039c5eabe0
Durbek <pattoyevd@gmail.com>
2022-12-29 15:37:49 +0500

    delete signup

--- comment/views.py
+++ comment/views.py
@@ -4,13 +4,5 @@ from .models import *
 def home(request):
     return render(request, "comment.html")
 
-def signup(request):
-    form = Registration()
-    if request.method == "POST":
-        form = Registration(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/login/')
-    
-    return render(request, 'registration/signup.html', {"form":form})
+
 
